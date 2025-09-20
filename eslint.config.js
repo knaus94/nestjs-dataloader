4226cded48f0a133880707c01b2613050fd1fa8a
knaus94 <dmitry.mahitov@gmail.com>
2025-09-20 15:12:35 +0300

    upd

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.cjs
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  { ignores: ['node_modules', 'dist'] },
+
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+    },
+  },
+
+  prettier,
+);
